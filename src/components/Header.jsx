345727c382112e646e26e88d97dab81a70183409
dpalexandru <alexpoian91@hotmail.com>
2025-08-29 15:21:57 +0200

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="container bg-light">
+      <header>
+        <img src="https://www.calciomagazine.net/wp-content/uploads/2020/05/Stemma-cagliari-e1588957815949.jpg" alt="logo cgliari calcio" />
+        <h1>Lista Articoli Cagliari Calcio</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
