3a2be5263257f4b0ef02881eded72a352b36dc9b
dpalexandru <alexpoian91@hotmail.com>
2025-08-29 15:25:59 +0200

    feat: add array whth articles of cagliaricalcio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
 
+// DATA 
+
+const articoliCagliari = [
+  "Cagliari Calcio: la nuova stagione tra sogni e obiettivi in Serie A",
+  "I giocatori simbolo del Cagliari: da Riva a oggi",
+  "Cagliari Calcio e la passione dei tifosi sardi: un amore senza confini",
+  "Analisi tattica: come gioca il Cagliari di quest’anno",
+  "Le partite storiche del Cagliari Calcio che i tifosi non dimenticheranno"
+];
+
 const App = () => {
   return (
     <>
